2709ea8d42875296ff48d409b0bd46bb3f43b21c
rabileon <rabi_leon@hotmail.com>
2025-05-10 22:08:20 -0600

    GetFiles BD

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getFiles } from '../controllers/files';
+import { getFiles } from '../controllers/files.js';
 const router = express.Router();
 
 router.get('/files', getFiles)
